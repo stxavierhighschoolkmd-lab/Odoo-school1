3b0e3129fce70cecf1f77d25a224e5908e474b92
Sébastien (blse) <blse@odoo.com>
2025-10-09 17:08:14 +0200

    [REM] website, html_builder, *: remove class `o_snippet_invisible`
    *: website_mass_mailing, website_sale
    
    With the previous commit, the class `o_snippet_invisible` is not used
    anymore
    
    task-5149984

--- addons/website/static/tests/tours/website_page_options.js
+++ addons/website/static/tests/tours/website_page_options.js
@@ -83,7 +83,7 @@ registerWebsitePreviewTour(
         ...clickOnSave(),
         {
             content: "Check that the header is hidden",
-            trigger: ":iframe #wrapwrap:has(header#top.d-none.o_snippet_invisible)",
+            trigger: ":iframe #wrapwrap:has(header#top.d-none)",
         },
         ...clickOnEditAndWaitEditMode(),
         {
@@ -105,7 +105,7 @@ registerWebsitePreviewTour(
         },
         {
             content: "Check that the footer is hidden",
-            trigger: ":iframe #wrapwrap:has(.o_footer.d-none.o_snippet_invisible)",
+            trigger: ":iframe #wrapwrap:has(.o_footer.d-none)",
         },
     ]
 );
@@ -277,7 +277,7 @@ registerWebsitePreviewTour(
         ...clickOnSave(),
         {
             content: "Verify that the breadcrumb is hidden",
-            trigger: ":iframe main:has(div.o_page_breadcrumb.d-none.o_snippet_invisible)",
+            trigger: ":iframe main:has(div.o_page_breadcrumb.d-none)",
         },
     ]
 );
