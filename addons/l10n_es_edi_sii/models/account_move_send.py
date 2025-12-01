97f0f18e37b9223246800e2d92f720e9309017ed
Smit Patel <pasm@odoo.com>
2025-12-01 11:30:34 +0530

    [REF] l10n_es_edi_sii: remove account_edi dependency
    This commit refactors the Spanish SII EDI implementation to remove the
    dependency on account_edi and align the flow with send and print wizard.
    
    Key changes:
    - Refactor SII service logic to work with send and print wizard
    - Improve tax aggregation and payload construction for single invoice
    - Update and fix tests accordingly
    
    upgrade pr- odoo/upgrade#9482
    task- 5190498

--- addons/l10n_es_edi_sii/models/account_move_send.py
+++ addons/l10n_es_edi_sii/models/account_move_send.py
@@ -1,10 +1,39 @@
-from odoo import models
+from odoo import api, models
 
 
 class AccountMoveSend(models.AbstractModel):
     _inherit = 'account.move.send'
 
-    def _get_mail_attachment_from_doc(self, doc):
-        if doc.name == 'jsondump.json' and doc.edi_format_id.code == 'es_sii':
-            return self.env['ir.attachment']
-        return super()._get_mail_attachment_from_doc(doc)
+    @api.model
+    def _l10n_es_is_edi_sii_applicable(self, move):
+        return move.l10n_es_edi_is_required and move.l10n_es_edi_sii_state != 'sent'
+
+    def _get_all_extra_edis(self) -> dict:
+        # EXTENDS 'account'
+        res = super()._get_all_extra_edis()
+        res.update({
+            'es_edi_sii': {
+                'label': self.env._("Send to SII"),
+                'is_applicable': self._l10n_es_is_edi_sii_applicable,
+                'help': self.env._("Send the e-invoice data to SII"),
+            }
+        })
+        return res
+
+    # -------------------------------------------------------------------------
+    # SENDING METHODS
+    # -------------------------------------------------------------------------
+
+    def _call_web_service_before_invoice_pdf_render(self, invoices_data):
+        super()._call_web_service_before_invoice_pdf_render(invoices_data)
+
+        for invoice, invoice_data in invoices_data.items():
+            if 'es_edi_sii' in invoice_data['extra_edis']:
+                invoice._send_l10n_es_sii_document()
+                if invoice.l10n_es_edi_sii_error:
+                    invoice_data['error'] = {
+                        'error_title': self.env._("Error while sending the invoice to SII"),
+                        'errors': [invoice.l10n_es_edi_sii_error],
+                    }
+                if self._can_commit():
+                    self.env.cr.commit()
