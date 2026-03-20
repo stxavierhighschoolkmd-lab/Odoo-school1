048cb75c2cb3b88f7488ea467703e41b70ec83b6
Garvish Panchal <gppa@odoo.com>
2026-03-20 18:06:14 +0530

    [REM] *: remove description and transform from image toolbar
    *=html_builder, html_editor, website_forum
    
    Removes `ImageDescription`, `ImageDescriptionPopover`,
    `ImageTransformButton` and related tests from html_editor's image
    toolbar.
    
    Migrates required image transform logic to html_builder.
    
    task-5068403

--- addons/html_editor/static/src/main/media/image_plugin.js
+++ addons/html_editor/static/src/main/media/image_plugin.js
@@ -2,22 +2,14 @@ import { reactive } from "@web/owl2/utils";
 import { Plugin } from "../../plugin";
 import { _t } from "@web/core/l10n/translation";
 import { isImageUrl } from "@html_editor/utils/url";
-import { ImageDescription, ImageDescriptionPopover } from "./image_description";
 import { ImageToolbarDropdown } from "./image_toolbar_dropdown";
 import { createFileViewer } from "@web/core/file_viewer/file_viewer_hook";
 import { isHtmlContentSupported } from "@html_editor/core/selection_plugin";
 import { boundariesOut } from "@html_editor/utils/position";
 import { READ, withSequence } from "@html_editor/utils/resource";
-import { ImageTransformButton } from "./image_transform_button";
 import { callbacksForCursorUpdate } from "@html_editor/utils/selection";
 import { closestBlock } from "@html_editor/utils/blocks";
 import { fillEmpty } from "@html_editor/utils/dom";
-
-function hasShape(imagePlugin, shapeName) {
-    return () => imagePlugin.isSelectionShaped(shapeName);
-}
-
-export const IMAGE_SHAPES = ["rounded", "rounded-circle", "shadow", "img-thumbnail"];
 
 const IMAGE_PADDING = [
     { name: "None", value: 0 },
@@ -38,7 +30,6 @@ const IMAGE_SIZE = [
  * @typedef { Object } ImageShared
  * @property { ImagePlugin['getTargetedImage'] } getTargetedImage
  * @property { ImagePlugin['previewImage'] } previewImage
- * @property { ImagePlugin['resetImageTransformation'] } resetImageTransformation
  */
 
 /**
@@ -48,9 +39,8 @@ const IMAGE_SIZE = [
 
 export class ImagePlugin extends Plugin {
     static id = "image";
-    static dependencies = ["history", "dom", "selection", "overlay"];
-    static shared = ["getTargetedImage", "previewImage", "resetImageTransformation"];
-    static defaultConfig = { allowImageTransform: true };
+    static dependencies = ["history", "dom", "selection"];
+    static shared = ["getTargetedImage", "previewImage"];
     toolbarNamespace = "image";
     /** @type {import("plugins").EditorResources} */
     resources = {
@@ -67,34 +57,6 @@ export class ImagePlugin extends Plugin {
                 description: _t("Preview image"),
                 icon: "fa-search-plus",
                 run: this.previewImage.bind(this),
-                isAvailable: isHtmlContentSupported,
-            },
-            {
-                id: "setImageShapeRounded",
-                description: _t("Set shape: Rounded"),
-                icon: "fa-square",
-                run: () => this.setImageShape("rounded", { excludeClasses: ["rounded-circle"] }),
-                isAvailable: isHtmlContentSupported,
-            },
-            {
-                id: "setImageShapeCircle",
-                description: _t("Set shape: Circle"),
-                icon: "fa-circle-o",
-                run: () => this.setImageShape("rounded-circle", { excludeClasses: ["rounded"] }),
-                isAvailable: isHtmlContentSupported,
-            },
-            {
-                id: "setImageShapeShadow",
-                description: _t("Set shape: Shadow"),
-                icon: "fa-sun-o",
-                run: () => this.setImageShape("shadow"),
-                isAvailable: isHtmlContentSupported,
-            },
-            {
-                id: "setImageShapeThumbnail",
-                description: _t("Set shape: Thumbnail"),
-                icon: "fa-picture-o",
-                run: () => this.setImageShape("img-thumbnail"),
                 isAvailable: isHtmlContentSupported,
             },
             {
@@ -115,8 +77,6 @@ export class ImagePlugin extends Plugin {
         ],
         toolbar_groups: [
             withSequence(23, { id: "image_preview", namespaces: ["image"] }),
-            withSequence(24, { id: "image_description", namespaces: ["image"] }),
-            withSequence(25, { id: "image_shape", namespaces: ["image"] }),
             withSequence(26, { id: "image_padding", namespaces: ["image"] }),
             withSequence(26, { id: "image_size", namespaces: ["image"] }),
             withSequence(26, { id: "image_modifiers", namespaces: ["image"] }),
@@ -127,40 +87,6 @@ export class ImagePlugin extends Plugin {
                 id: "image_preview",
                 groupId: "image_preview",
                 commandId: "previewImage",
-            },
-            {
-                id: "image_description",
-                description: _t("Edit media description"),
-                groupId: "image_description",
-                Component: ImageDescription,
-                props: {
-                    openImageDescriptionPopover: this.openImageDescriptionPopover.bind(this),
-                },
-                isAvailable: isHtmlContentSupported,
-            },
-            {
-                id: "shape_rounded",
-                groupId: "image_shape",
-                commandId: "setImageShapeRounded",
-                isActive: hasShape(this, "rounded"),
-            },
-            {
-                id: "shape_circle",
-                groupId: "image_shape",
-                commandId: "setImageShapeCircle",
-                isActive: hasShape(this, "rounded-circle"),
-            },
-            {
-                id: "shape_shadow",
-                groupId: "image_shape",
-                commandId: "setImageShapeShadow",
-                isActive: hasShape(this, "shadow"),
-            },
-            {
-                id: "shape_thumbnail",
-                groupId: "image_shape",
-                commandId: "setImageShapeThumbnail",
-                isActive: hasShape(this, "img-thumbnail"),
             },
             {
                 id: "image_padding",
@@ -195,15 +121,6 @@ export class ImagePlugin extends Plugin {
                     isHtmlContentSupported(selection) && (this.config.allowImageResize ?? true),
             },
             {
-                id: "image_transform",
-                groupId: "image_modifiers",
-                description: _t("Transform the picture (click twice to reset transformation)"),
-                Component: ImageTransformButton,
-                props: this.getImageTransformProps(),
-                isAvailable: (selection) =>
-                    this.config.allowImageTransform && isHtmlContentSupported(selection),
-            },
-            {
                 id: "image_delete",
                 groupId: "image_delete",
                 commandId: "deleteImage",
@@ -214,13 +131,6 @@ export class ImagePlugin extends Plugin {
         on_selectionchange_handlers: withSequence(READ, this.updateImageParams.bind(this)),
         on_undone_handlers: this.updateImageParams.bind(this),
         on_redone_handlers: this.updateImageParams.bind(this),
-        on_will_save_media_dialog_handlers: async (elements) => {
-            for (const element of elements) {
-                if (element && element.tagName === "IMG") {
-                    this.resetImageTransformation(element, { addStep: false });
-                }
-            }
-        },
 
         /** Providers */
         paste_media_url_command_providers: this.getCommandForImageUrlPaste.bind(this),
@@ -240,9 +150,6 @@ export class ImagePlugin extends Plugin {
             }
         });
         this.fileViewer = createFileViewer();
-        this.overlay = this.dependencies.overlay.createOverlay(ImageDescriptionPopover, {
-            className: "popover",
-        });
     }
 
     destroy() {
@@ -276,20 +183,6 @@ export class ImagePlugin extends Plugin {
             return;
         }
         targetedImg.style.width = size || "";
-        this.dependencies.history.addStep();
-    }
-
-    setImageShape(className, { excludeClasses = [] } = {}) {
-        const targetedImg = this.getTargetedImage();
-        if (!targetedImg) {
-            return;
-        }
-        for (const classString of excludeClasses) {
-            if (targetedImg.classList.contains(classString)) {
-                targetedImg.classList.remove(classString);
-            }
-        }
-        targetedImg.classList.toggle(className);
         this.dependencies.history.addStep();
     }
 
@@ -348,12 +241,6 @@ export class ImagePlugin extends Plugin {
         return targetedNodes.length > 0;
     }
 
-    getImageAttribute(attributeName) {
-        const targetedNodes = this.dependencies.selection.getTargetedNodes();
-        const targetedImg = targetedNodes.find((node) => node.tagName === "IMG");
-        return targetedImg.getAttribute(attributeName) || undefined;
-    }
-
     /**
      * @param {string} url
      */
@@ -377,66 +264,8 @@ export class ImagePlugin extends Plugin {
         }
     }
 
-    updateImageDescription({ description, tooltip } = {}) {
-        const targetedImg = this.getTargetedImage();
-        if (!targetedImg) {
-            return;
-        }
-        targetedImg.setAttribute("alt", description);
-        targetedImg.setAttribute("title", tooltip);
-        this.dependencies.history.addStep();
-    }
-
-    resetImageTransformation(image, { addStep = true } = {}) {
-        const stylePropertiesToRemove = [
-            "transform",
-            "transform-box",
-            "transform-origin",
-            "transform-style",
-            "width",
-            "height",
-        ];
-        for (const styleProperty of stylePropertiesToRemove) {
-            image.style.removeProperty(styleProperty);
-        }
-        if (addStep) {
-            this.dependencies.history.addStep();
-        }
-    }
-
-    getImageTransformProps() {
-        return {
-            id: "image_transform",
-            icon: "fa-object-ungroup",
-            title: _t("Transform the picture (click twice to reset transformation)"),
-            getTargetedImage: this.getTargetedImage.bind(this),
-            resetImageTransformation: this.resetImageTransformation.bind(this),
-            addStep: this.dependencies.history.addStep.bind(this),
-            document: this.document,
-            editable: this.editable,
-            activeTitle: _t("Click again to reset transformation"),
-        };
-    }
-
     updateImageParams() {
         this.imageSize.displayName = this.imageSizeName;
-    }
-
-    openImageDescriptionPopover() {
-        const image = this.getTargetedImage();
-        if (image) {
-            this.overlay.open({
-                target: image,
-                props: {
-                    close: () => this.overlay.close(),
-                    description: this.getImageAttribute("alt"),
-                    tooltip: this.getImageAttribute("title"),
-                    onConfirm: (description, tooltip) => {
-                        this.updateImageDescription({ description, tooltip });
-                    },
-                },
-            });
-        }
     }
 
     setSelectionAroundImage(img) {
