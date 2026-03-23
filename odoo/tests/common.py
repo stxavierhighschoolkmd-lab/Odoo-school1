d35271fd4522d5a08b673e851caf6b3bae469cf4
Krzysztof Magusiak (krma) <krma@odoo.com>
2026-03-23 12:43:00 +0000

    [IMP] tests: patch get_sequences in test mode
    Since we are in test mode, signaling is not needed because we use just
    one real cursor. This makes sequences consistent with signaling in
    tests.

--- odoo/tests/common.py
+++ odoo/tests/common.py
@@ -1086,12 +1086,16 @@ class BaseCase(case.TestCase):
                 cr, _registry_test_lock, readonly and cls._registry_readonly_enabled
             )
 
+        def get_sequences(cr):
+            return registry.registry_sequence, registry.cache_sequences.copy()
+
         return [
             # New cursor should point to the test's cursor
             patch.object(registry, 'cursor', _patched_cursor),
             # Disable locking and signaling
             patch.object(Registry, '_lock', DummyRLock()),
             patch.object(registry, 'setup_signaling', return_value=None),  # noop
+            patch.object(registry, 'get_sequences', get_sequences),
         ]
 
     @classmethod
