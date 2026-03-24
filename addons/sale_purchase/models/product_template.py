odoo/odoo

[IMP] sale_purchase: make subcontract service option easier to discover
In Odoo, it was already possible to configure a service product to generate a
subcontracting RFQ upon Sales Order confirmation. However, this option
was hard to discover, as it was located in the Purchase tab.

To improve usability, this option has been moved to the 'Create on Order
selection field' in the General Information tab, alongside other similar actions,
making it more visible and accessible.

Additionally, the 'service_tracking' field has been made 'company-dependent'
to align with the existing behaviour of the 'service_to_purchase' field and
to ensure consistent functionality across companies.

Overall, this enhancement centralises subcontract service configuration for
service-type products and improves clarity and usability.

Related test cases have been updated to reflect the new logic.

Task ID: 5249083

addons/sale_purchase/models/product_template.py
@@ -1,37 +1,53 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import api, fields, models, _
+from odoo import api, fields, models
 from odoo.exceptions import ValidationError
 
 
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
 
-    service_to_purchase = fields.Boolean(
-        "Subcontract Service", company_dependent=True, copy=False,
-        help="If ticked, each time you sell this product through a SO, a RfQ is automatically created to buy the product. Tip: don't forget to set a vendor on the product.")
+    service_tracking = fields.Selection(selection_add=[
+        ('subcontract', 'Subcontracting RFQ')
+    ], ondelete={'subcontract': 'set default'})
 
-    @api.constrains('service_to_purchase', 'seller_ids', 'type')
-    def _check_service_to_purchase(self):
+    @api.depends('service_tracking')
+    def _compute_purchase_ok(self):
+        super()._compute_purchase_ok()
+        self.filtered(lambda t: t.service_tracking == 'subcontract' and not t.purchase_ok).purchase_ok = True
+
+    @api.depends('purchase_ok', 'expense_policy')
+    def _compute_service_tracking(self):
+        super()._compute_service_tracking()
+        self.filtered(lambda t: t.service_tracking == 'subcontract' and (not t.purchase_ok or t.expense_policy != 'no')).service_tracking = 'no'
+
+    @api.model
+    def _get_saleable_tracking_types(self):
+        return super()._get_saleable_tracking_types() + ['subcontract']
+
+    @api.model
+    def _get_product_types_allow_zero_price(self):
+        return super()._get_product_types_allow_zero_price() + ['subcontract']
+
+    @api.constrains('service_tracking', 'seller_ids')
+    def _check_service_tracking_subcontract(self):
         for template in self:
-            if template.service_to_purchase:
-                if template.type != 'service':
-                    raise ValidationError(_("Product that is not a service can not create RFQ."))
-                template._check_vendor_for_service_to_purchase(template.seller_ids)
+            if template.service_tracking == 'subcontract':
+                template._check_vendor_for_service_tracking_subcontract(template.seller_ids)
 
     @api.model_create_multi
     def create(self, vals_list):
         for vals in vals_list:
-            if vals.get('service_to_purchase'):
-                self._check_vendor_for_service_to_purchase(vals.get('seller_ids'))
+            if vals.get('service_tracking') == 'subcontract':
+                self._check_vendor_for_service_tracking_subcontract(vals.get('seller_ids'))
         return super().create(vals_list)
 
-    def _check_vendor_for_service_to_purchase(self, sellers):
-        if not sellers:
-            raise ValidationError(_("Please define the vendor from whom you would like to purchase this service automatically."))
+    def _prepare_service_tracking_tooltip(self):
+        if self.service_tracking == 'subcontract':
+            return self.env._("Each time you sell this product through a SO, a RfQ is automatically created to buy the product. Tip: don't forget to set a vendor on the product.")
+        return super()._prepare_service_tracking_tooltip()
 
-    @api.onchange('type', 'expense_policy')
-    def _onchange_service_to_purchase(self):
-        products_template = self.filtered(lambda p: p.type != 'service' or p.expense_policy != 'no')
-        products_template.service_to_purchase = False
+    def _check_vendor_for_service_tracking_subcontract(self, sellers):
+        if not sellers:
+            raise ValidationError(self.env._("Please define the vendor from whom you would like to purchase this service automatically."))

stxavierhighschoolkmd-lab/Odoo-school1

addons/sale_purchase/models/product_template.py
@@ -1,37 +1,53 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import api, fields, models, _
+from odoo import api, fields, models
 from odoo.exceptions import ValidationError
 
 
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
 
-    service_to_purchase = fields.Boolean(
-        "Subcontract Service", company_dependent=True, copy=False,
-        help="If ticked, each time you sell this product through a SO, a RfQ is automatically created to buy the product. Tip: don't forget to set a vendor on the product.")
+    service_tracking = fields.Selection(selection_add=[
+        ('subcontract', 'Subcontracting RFQ')
+    ], ondelete={'subcontract': 'set default'})
 
-    @api.constrains('service_to_purchase', 'seller_ids', 'type')
-    def _check_service_to_purchase(self):
+    @api.depends('service_tracking')
+    def _compute_purchase_ok(self):
+        super()._compute_purchase_ok()
+        self.filtered(lambda t: t.service_tracking == 'subcontract' and not t.purchase_ok).purchase_ok = True
+
+    @api.depends('purchase_ok', 'reinvoice_policy')
+    def _compute_service_tracking(self):
+        super()._compute_service_tracking()
+        self.filtered(lambda t: t.service_tracking == 'subcontract' and (not t.purchase_ok or t.reinvoice_policy != 'no')).service_tracking = 'no'
+
+    @api.model
+    def _get_saleable_tracking_types(self):
+        return super()._get_saleable_tracking_types() + ['subcontract']
+
+    @api.model
+    def _get_product_types_allow_zero_price(self):
+        return super()._get_product_types_allow_zero_price() + ['subcontract']
+
+    @api.constrains('service_tracking', 'seller_ids')
+    def _check_service_tracking_subcontract(self):
         for template in self:
-            if template.service_to_purchase:
-                if template.type != 'service':
-                    raise ValidationError(_("Product that is not a service can not create RFQ."))
-                template._check_vendor_for_service_to_purchase(template.seller_ids)
+            if template.service_tracking == 'subcontract':
+                template._check_vendor_for_service_tracking_subcontract(template.seller_ids)
 
     @api.model_create_multi
     def create(self, vals_list):
-        for vals in vals_list:
-            if vals.get('service_to_purchase'):
-                self._check_vendor_for_service_to_purchase(vals.get('seller_ids'))
-        return super().create(vals_list)
+        templates = super().create(vals_list)
+        for template in templates.filtered(lambda t: t.service_tracking == 'subcontract'):
+            self._check_vendor_for_service_tracking_subcontract(template.seller_ids)
+        return templates
 
-    def _check_vendor_for_service_to_purchase(self, sellers):
+    def _prepare_service_tracking_tooltip(self):
+        if self.service_tracking == 'subcontract':
+            return self.env._("Each time you sell this product through a SO, a RfQ is automatically created to buy the product. Tip: don't forget to set a vendor on the product.")
+        return super()._prepare_service_tracking_tooltip()
+
+    def _check_vendor_for_service_tracking_subcontract(self, sellers):
         if not sellers:
-            raise ValidationError(_("Please define the vendor from whom you would like to purchase this service automatically."))
-
-    @api.onchange('type', 'reinvoice_policy')
-    def _onchange_service_to_purchase(self):
-        products_template = self.filtered(lambda p: p.type != 'service' or p.reinvoice_policy != 'no')
-        products_template.service_to_purchase = False
+            raise ValidationError(self.env._("Please define the vendor from whom you would like to purchase this service automatically."))