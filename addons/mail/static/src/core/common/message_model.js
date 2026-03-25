odoo/odoo

[IMP] mail,*: preserve the message formatting in various message preview
*=html_editor

Before this commit,
The message preview that was shown in various parts of the applications shows
only the inline version of the message with formatting being stripped out.
This gave false impression about how the original message actually is...

This commit, retains the message's original formatting and making it inline
by adding appropriate spaces in place of <br> and in between two block level
elements.

Task-5263284

addons/mail/static/src/core/common/message_model.js
@@ -8,6 +8,7 @@ import {
     generateEmojisOnHtml,
     getNonEditableMentions,
     htmlToTextContentInline,
+    normalizeBrAndBlocks,
 } from "@mail/utils/common/format";
 
 import { browser } from "@web/core/browser/browser";
@@ -408,7 +409,29 @@ export class Message extends Record {
         return false;
     }
 
-    inlineBody = fields.Html("", {
+    notificationBody = fields.Html("", {
+        /** @this {import("models").Message} */
+        compute() {
+            switch (this.notificationType) {
+                case "call":
+                    return _t("%(caller)s started a call", { caller: this.authorName });
+                case "thread_deletion":
+                    return _t('%(user)s deleted the thread "%(thread_name)s"', {
+                        user: this.authorName,
+                        thread_name: decorateEmojis(htmlToTextContentInline(this.body)),
+                    });
+                case "channel_rename":
+                    const name = htmlToTextContentInline(this.body);
+                    const params = { user: this.authorName, name: markup`<b>${name}</b>` };
+                    return this.thread?.channel?.parent_channel_id
+                        ? _t("%(user)s changed the thread name to %(name)s", params)
+                        : _t("%(user)s changed the channel name to %(name)s", params);
+            }
+            return htmlToTextContentInline(this.body);
+        },
+    });
+
+    richInlineBody = fields.Html("", {
         /** @this {import("models").Message} */
         compute() {
             if (this.poll) {
@@ -418,29 +441,13 @@ export class Message extends Record {
                 }
                 return markup`<i class="fa fa-fw o-me-0_5 oi oi-view-cohort"></i>${text}`;
             }
-            if (this.notificationType === "call") {
-                return _t("%(caller)s started a call", { caller: this.authorName });
-            }
-            if (this.notificationType === "thread_deletion") {
-                return _t('%(user)s deleted the thread "%(thread_name)s"', {
-                    user: this.authorName,
-                    thread_name: decorateEmojis(htmlToTextContentInline(this.body)),
-                });
-            }
-            if (this.notificationType === "channel_rename") {
-                const name = htmlToTextContentInline(this.body);
-                const params = { user: this.authorName, name: markup`<b>${name}</b>` };
-                return this.thread?.channel?.parent_channel_id
-                    ? _t("%(user)s changed the thread name to %(name)s", params)
-                    : _t("%(user)s changed the channel name to %(name)s", params);
-            }
             if (this.isEmpty) {
                 return _t("This message has been removed");
             }
             if (!this.body) {
                 return "";
             }
-            return decorateEmojis(htmlToTextContentInline(this.body));
+            return normalizeBrAndBlocks(this.richBody);
         },
     });
 
@@ -487,7 +494,7 @@ export class Message extends Record {
         compute() {
             let messageBody = "";
             if (!this.hasOnlyAttachments) {
-                return this.inlineBody || this.subtype_id?.description;
+                return this.richInlineBody || this.subtype_id?.description;
             }
             const attachments = this.attachment_ids;
             switch (attachments.length) {

addons/mail/static/src/utils/common/format.js
@@ -10,6 +10,7 @@ import { getInnerHtml, getOuterHtml } from "@mail/utils/common/html";
 
 import { htmlEscape, markup } from "@odoo/owl";
 
+import { blockTagNames } from "@html_editor/utils/blocks";
 import { router } from "@web/core/browser/router";
 import { loadEmoji, loader } from "@web/core/emoji_picker/emoji_picker";
 import { normalize } from "@web/core/l10n/utils";
@@ -368,6 +369,30 @@ export function htmlToTextContentInline(htmlString) {
 export function convertBrToLineBreak(str) {
     str = htmlReplace(str, /<br\s*\/?>/gi, () => "\n");
     return createDocumentFragmentFromContent(str).body.textContent;
+}
+
+/**
+ * @param {string|ReturnType<markup>} richbody
+ * @returns {ReturnType<markup>}
+ */
+export function normalizeBrAndBlocks(richbody) {
+    const doc = createDocumentFragmentFromContent(richbody);
+    const body = doc.body;
+    const lineBreaks = body.querySelectorAll("br");
+    const blockChildren = Array.from(body.children);
+    if (lineBreaks.length || blockChildren.length > 1) {
+        for (const br of lineBreaks) {
+            br.replaceWith(body.ownerDocument.createTextNode("\u00A0"));
+        }
+        for (let i = blockChildren.length - 1; i > 0; i--) {
+            const prev = blockChildren[i - 1];
+            const curr = blockChildren[i];
+            if (blockTagNames.includes(prev.tagName) && blockTagNames.includes(curr.tagName)) {
+                body.insertBefore(body.ownerDocument.createTextNode("\u00A0"), curr);
+            }
+        }
+    }
+    return markup(body.innerHTML.trim()) ?? "";
 }
 
 export function cleanTerm(term) {

stxavierhighschoolkmd-lab/Odoo-school1

addons/mail/static/src/core/common/message_model.js
@@ -8,6 +8,7 @@ import {
     generateEmojisOnHtml,
     getNonEditableMentions,
     htmlToTextContentInline,
+    htmlToHtmlInline,
 } from "@mail/utils/common/format";
 
 import { browser } from "@web/core/browser/browser";
@@ -448,7 +449,29 @@ export class Message extends Record {
         return false;
     }
 
-    inlineBody = fields.Html("", {
+    inlineTextBody = fields.Html("", {
+        /** @this {import("models").Message} */
+        compute() {
+            switch (this.notificationType) {
+                case "call":
+                    return _t("%(caller)s started a call", { caller: this.authorName });
+                case "thread_deletion":
+                    return _t('%(user)s deleted the thread "%(thread_name)s"', {
+                        user: this.authorName,
+                        thread_name: decorateEmojis(htmlToTextContentInline(this.body)),
+                    });
+                case "channel_rename":
+                    const name = htmlToTextContentInline(this.body);
+                    const params = { user: this.authorName, name: markup`<b>${name}</b>` };
+                    return this.thread?.channel?.parent_channel_id
+                        ? _t("%(user)s changed the thread name to %(name)s", params)
+                        : _t("%(user)s changed the channel name to %(name)s", params);
+            }
+            return htmlToTextContentInline(this.body);
+        },
+    });
+
+    inlineRichBody = fields.Html("", {
         /** @this {import("models").Message} */
         compute() {
             if (this.poll) {
@@ -458,29 +481,13 @@ export class Message extends Record {
                 }
                 return markup`<i class="fa fa-fw o-me-0_5 oi oi-view-cohort"></i>${text}`;
             }
-            if (this.notificationType === "call") {
-                return _t("%(caller)s started a call", { caller: this.authorName });
-            }
-            if (this.notificationType === "thread_deletion") {
-                return _t('%(user)s deleted the thread "%(thread_name)s"', {
-                    user: this.authorName,
-                    thread_name: decorateEmojis(htmlToTextContentInline(this.body)),
-                });
-            }
-            if (this.notificationType === "channel_rename") {
-                const name = htmlToTextContentInline(this.body);
-                const params = { user: this.authorName, name: markup`<b>${name}</b>` };
-                return this.thread?.channel?.parent_channel_id
-                    ? _t("%(user)s changed the thread name to %(name)s", params)
-                    : _t("%(user)s changed the channel name to %(name)s", params);
-            }
             if (this.isEmpty) {
                 return _t("This message has been removed");
             }
             if (!this.body) {
                 return "";
             }
-            return decorateEmojis(htmlToTextContentInline(this.body));
+            return htmlToHtmlInline(this.richBody);
         },
     });
 
@@ -526,7 +533,7 @@ export class Message extends Record {
         compute() {
             let messageBody = "";
             if (!this.hasOnlyAttachments) {
-                return this.inlineBody || this.subtype_id?.description;
+                return this.inlineRichBody || this.subtype_id?.description;
             }
             const attachments = this.attachment_ids;
             switch (attachments.length) {

addons/mail/static/src/utils/common/format.js
@@ -10,6 +10,7 @@ import { getInnerHtml, getOuterHtml } from "@mail/utils/common/html";
 
 import { htmlEscape, markup } from "@odoo/owl";
 
+import { blockTagNames } from "@html_editor/utils/blocks";
 import { router } from "@web/core/browser/router";
 import { emojiLoader } from "@web/core/emoji_picker/emoji_loader";
 import { formatList, normalize } from "@web/core/l10n/utils";
@@ -370,6 +371,30 @@ export function convertBrToLineBreak(str) {
 
 export function convertLineBreakToBr(str) {
     return htmlReplace(str, /(\r|\n)/g, () => markup`<br/>`);
+}
+
+/**
+ * @param {string|ReturnType<markup>} richbody
+ * @returns {ReturnType<markup>}
+ */
+export function htmlToHtmlInline(richbody) {
+    const doc = createDocumentFragmentFromContent(richbody);
+    const body = doc.body;
+    const lineBreaks = body.querySelectorAll("br");
+    const blockChildren = Array.from(body.children);
+    if (lineBreaks.length || blockChildren.length > 1) {
+        for (const br of lineBreaks) {
+            br.replaceWith(body.ownerDocument.createTextNode("\u00A0"));
+        }
+        for (let i = blockChildren.length - 1; i > 0; i--) {
+            const prev = blockChildren[i - 1];
+            const curr = blockChildren[i];
+            if (blockTagNames.includes(prev.tagName) && blockTagNames.includes(curr.tagName)) {
+                body.insertBefore(body.ownerDocument.createTextNode("\u00A0"), curr);
+            }
+        }
+    }
+    return htmlTrim(getInnerHtml(body)) ?? "";
 }
 
 export function cleanTerm(term) {
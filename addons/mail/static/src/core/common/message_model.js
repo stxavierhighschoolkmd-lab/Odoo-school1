8beed1cde59495781cbcfd96f6ff439b42afe620
maad-odoo <maad@odoo.com>
2025-12-09 12:41:32 +0530

    [IMP] mail,*: preserve the message formatting in various message preview
    *=html_editor
    
    Before this commit,
    The message preview that was shown in various parts of the applications shows
    only the inline version of the message with formatting being stripped out.
    This gave false impression about how the original message actually is...
    
    This commit, retains the message's original formatting and making it inline
    by adding appropriate spaces in place of <br> and in between two block level
    elements.
    
    Task-5263284

--- addons/mail/static/src/core/common/message_model.js
+++ addons/mail/static/src/core/common/message_model.js
@@ -8,6 +8,7 @@ import {
     generateEmojisOnHtml,
     getNonEditableMentions,
     htmlToTextContentInline,
+    htmlToHtmlInline,
 } from "@mail/utils/common/format";
 
 import { browser } from "@web/core/browser/browser";
@@ -448,7 +449,29 @@ export class Message extends Record {
         return false;
     }
 
-    inlineBody = fields.Html("", {
+    inlineTextBody = fields.Html("", {
+        /** @this {import("models").Message} */
+        compute() {
+            switch (this.notificationType) {
+                case "call":
+                    return _t("%(caller)s started a call", { caller: this.authorName });
+                case "thread_deletion":
+                    return _t('%(user)s deleted the thread "%(thread_name)s"', {
+                        user: this.authorName,
+                        thread_name: decorateEmojis(htmlToTextContentInline(this.body)),
+                    });
+                case "channel_rename":
+                    const name = htmlToTextContentInline(this.body);
+                    const params = { user: this.authorName, name: markup`<b>${name}</b>` };
+                    return this.thread?.channel?.parent_channel_id
+                        ? _t("%(user)s changed the thread name to %(name)s", params)
+                        : _t("%(user)s changed the channel name to %(name)s", params);
+            }
+            return htmlToTextContentInline(this.body);
+        },
+    });
+
+    inlineRichBody = fields.Html("", {
         /** @this {import("models").Message} */
         compute() {
             if (this.poll) {
@@ -458,29 +481,13 @@ export class Message extends Record {
                 }
                 return markup`<i class="fa fa-fw o-me-0_5 oi oi-view-cohort"></i>${text}`;
             }
-            if (this.notificationType === "call") {
-                return _t("%(caller)s started a call", { caller: this.authorName });
-            }
-            if (this.notificationType === "thread_deletion") {
-                return _t('%(user)s deleted the thread "%(thread_name)s"', {
-                    user: this.authorName,
-                    thread_name: decorateEmojis(htmlToTextContentInline(this.body)),
-                });
-            }
-            if (this.notificationType === "channel_rename") {
-                const name = htmlToTextContentInline(this.body);
-                const params = { user: this.authorName, name: markup`<b>${name}</b>` };
-                return this.thread?.channel?.parent_channel_id
-                    ? _t("%(user)s changed the thread name to %(name)s", params)
-                    : _t("%(user)s changed the channel name to %(name)s", params);
-            }
             if (this.isEmpty) {
                 return _t("This message has been removed");
             }
             if (!this.body) {
                 return "";
             }
-            return decorateEmojis(htmlToTextContentInline(this.body));
+            return htmlToHtmlInline(this.richBody);
         },
     });
 
@@ -526,7 +533,7 @@ export class Message extends Record {
         compute() {
             let messageBody = "";
             if (!this.hasOnlyAttachments) {
-                return this.inlineBody || this.subtype_id?.description;
+                return this.inlineRichBody || this.subtype_id?.description;
             }
             const attachments = this.attachment_ids;
             switch (attachments.length) {
