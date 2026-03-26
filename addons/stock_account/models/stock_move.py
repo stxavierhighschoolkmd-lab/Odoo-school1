267cf7a0cf5f386e59f7b1f5a5ae8ac770d4edc1
plha-odoo <plha@odoo.com>
2026-03-26 09:52:02 +0000

    [FIX] stock_account,sale_stock_margin: compute cost on sale order dropshipped
    **Problem:**
    The cost is not correctly computed on sale order
    line when the product is dropshipped.
    
    **Steps to reproduce:**
    - enable "margins" and "dropshipping" settings
    - create a tracked, fifo product with dropship route
    - add a vendor in the purchase tab
    - confirm a sale order for 1 unit
    - set a unit price of 10 in the PO and confirm it
    - validate the dropship picking
    - come back to the sale order and unhide de cost column
    
    **Current behavior:**
    the cost is 0
    
    **Expected behavior:**
    the cost should be 10 based on the unit price of the PO
    
    **Cause of the issue:**
    To compute the purchase price, when there is valued moves
    linked to the sale order line and the product is fifo/avco,
    we call _get_price_unit() on the moves.
    https://github.com/odoo/odoo/blob/98e6e929bf8e0c34ec77fb9d07ef253e0abf681c/addons/sale_stock_margin/models/sale_order_line.py#L21
    Which uses the value of the moves
    https://github.com/odoo/odoo/blob/98e6e929bf8e0c34ec77fb9d07ef253e0abf681c/addons/stock_account/models/stock_move.py#L237-L243
    But for dropshipped move the value on the moves is always 0.
    So the return value will be 0 and purchase price will be 0.
    
    **fix:**
    - The idea of the fix is to use _get_value() instead of the move
    value for dropship moves.
    This approach is already used in the code inside
    _run_average_batch()
    https://github.com/odoo/odoo/blob/98e6e929bf8e0c34ec77fb9d07ef253e0abf681c/addons/stock_account/models/product.py#L474-L475
    
    - In case there is not only dropship moves we need to do
    a weighted average
    
    opw-6051004
    
    X-original-commit: 47473d31ca55519402abed7e6bb094fc2b29f6d3

--- addons/stock_account/models/stock_move.py
+++ addons/stock_account/models/stock_move.py
@@ -639,3 +639,34 @@ class StockMove(models.Model):
         if valued_type == 'out':
             return self.location_dest_id and self.location_dest_id.usage == 'supplier'
         return bool(self.picking_id.return_picking_id)
+<<<<<<< 442131acce0a8a447cbe6494465e838ebc35a7ef
+||||||| 8fc2f95276fae5830b8a0d66d2d15300b9288759
+
+    def _get_valued_consigned_qty(self):
+        return sum(self.move_line_ids.filtered(lambda l: l._is_consigned_valued_line()).mapped('quantity_product_uom'))
+=======
+
+    def _get_valued_consigned_qty(self):
+        return sum(self.move_line_ids.filtered(lambda l: l._is_consigned_valued_line()).mapped('quantity_product_uom'))
+
+    def _get_price_unit_delivery(self):
+        """ Computes the unit price for a set of moves, using a weighted average between
+        dropshipped and non dropshipped moves.
+        """
+        dropship_moves = self.filtered(lambda m: m._is_dropshipped() or m._is_dropshipped_returned())
+        dropship_quantity = sum(m._get_valued_qty() for m in dropship_moves)
+        dropship_price_unit = dropship_moves._get_price_unit_dropshipped()
+        regular_moves = self - dropship_moves
+        regular_quantity = sum(m._get_valued_qty() for m in regular_moves)
+        regular_price_unit = regular_moves._get_price_unit()
+        total_quantity = dropship_quantity + regular_quantity
+        if not total_quantity:
+            return self._get_price_unit()
+        return (dropship_quantity * dropship_price_unit + regular_quantity * regular_price_unit) / total_quantity
+
+    def _get_price_unit_dropshipped(self):
+        """ Returns the unit price to value the dropshipped moves."""
+        total_value = sum(m._get_value() for m in self)
+        total_qty = sum(m._get_valued_qty() for m in self)
+        return total_value / total_qty if total_qty else 0
+>>>>>>> dd30764bd287d9f03a94b2bc529e4e6d2bdaa94b
