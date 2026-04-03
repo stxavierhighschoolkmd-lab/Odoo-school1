2495ff72acd5be54ff247be94dceec51ee6a9a8a
Abdullah (abye) <abye@odoo.com>
2026-04-03 19:09:38 +0200

    [IMP] project_*: project margins followup
    - adjusted stat buttons in project form view:
      - reorder buttons
      - change the `Sale Order` stat button
      - modify the text of `Timesheets`
      - adjusted the `Status` button
      - changed the `actual margin` and `projected margin` into cost stat buttons
    
    - move and scale the favorite button in project form view
    - adjusted the real margins view
    - added more filters and groups to `sale.order.line` search record
    - remove uom and currency change calculation from `sale.report`
    
    task-6085988

--- addons/sale_project_margin/models/project_project.py
+++ addons/sale_project_margin/models/project_project.py
@@ -6,17 +6,27 @@ from odoo import fields, models
 class ProjectProject(models.Model):
     _inherit = 'project.project'
 
-    projected_margin = fields.Monetary(compute='_compute_projected_margin', export_string_translation=False)
+    estimated_cost = fields.Monetary(compute='_compute_estimated_cost', export_string_translation=False)
+    estimated_cost_ratio = fields.Float(compute='_compute_estimated_cost', export_string_translation=False)
 
-    def _compute_projected_margin(self):
-        all_sale_orders = self._fetch_sale_order_items({'project.task': [('is_closed', '=', False)]}).sudo().order_id
-        margin_per_project = dict(self.env['sale.order']._read_group(
-            domain=self._get_sale_orders_domain(all_sale_orders),
-            groupby=['project_id'],
-            aggregates=['margin:sum'],
-        ))
+    def _get_sol_cost_data_per_project(self, order_lines):
+        query = """
+            SELECT
+                SUM (purchase_price * product_uom_qty) AS cost,
+                SUM (price_subtotal) AS sold
+            FROM sale_order_line
+            WHERE id IN %s
+        """
+        self.env.cr.execute(query, (tuple(order_lines.ids),))
+        return self.env.cr.fetchone()
+
+    def _compute_estimated_cost(self):
+        all_sale_orders_lines = self._fetch_sale_order_items({'project.task': [('is_closed', '=', False)]})
+        costs_per_project = self._get_sol_cost_data_per_project(all_sale_orders_lines) if all_sale_orders_lines else (0, 0)
         for project in self:
-            project.projected_margin = margin_per_project.get(project, 0.0)
+            cost, sold = costs_per_project
+            project.estimated_cost = cost
+            project.estimated_cost_ratio = 100 * (project.estimated_cost / sold) if sold else 0.0
 
     def action_projected_margin(self):
         self.ensure_one()
@@ -24,6 +34,6 @@ class ProjectProject(models.Model):
         all_sale_orders_lines = self._fetch_sale_order_items({'project.task': [('is_closed', '=', False)]})
         action["domain"] = [("id", "in", all_sale_orders_lines.ids)]
         context = ast.literal_eval(action.get('context', '{}'))
-        context.update({'search_default_Customer': 0})
+        context.update({'search_default_customer': 0})
         action['context'] = context
         return action
