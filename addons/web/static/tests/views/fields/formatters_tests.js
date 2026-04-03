be6f6bb4aa940b2f1b1b3d46bf3756944fbf16b6
Samuel Degueldre <sad@odoo.com>
2026-04-03 10:05:24 +0200

    [FIX] web: fix some edge cases in the display of file sizes
    The generally accepted symbol for bytes is an uppercase B, while a
    lowercase b generally designates bits and is largely used when referring
    to data transfer rates (kbps, Mbps, etc.). In a similar vein, the
    commonly accepted prefix for kilo is a lowercase k.
    
    This commit changes the humanNumber util used for formatting sizes to
    use the generally accepted uppercase B and lowercase k.
    
    It also makes it so that the binary field formatter calls this util
    instead of reimplementing it on top of humanNumber. In this specific
    case it also fixes a minor issue where the result of dividing the length
    of the Base64 encoded string could result in fractional number of bytes,
    which doesn't make sense for file sizes, and changes the division by
    1.37 to a multiplication by .75, the 1.37 figure is specifically for
    Base64 encoded files in email that have a max line length of 76, which
    is irrelevant for this case. The size is still approximate because of
    padding.
    
    Lastly, we do not display .00 after numbers with a unit of "Bytes" as
    these digits are not significant (because again, fractional bytes don't
    make sense for file sizes)

--- addons/web/static/tests/views/fields/formatters_tests.js
+++ addons/web/static/tests/views/fields/formatters_tests.js
@@ -17,6 +17,7 @@ import {
     formatReference,
     formatText,
     formatX2many,
+    formatBinary,
 } from "@web/views/fields/formatters";
 
 QUnit.module("Fields", (hooks) => {
@@ -160,7 +161,10 @@ QUnit.module("Fields", (hooks) => {
         };
         assert.deepEqual(formatMonetary(200, { field, currencyId: 10, data }), "200.00\u00a0€");
         assert.deepEqual(formatMonetary(200, { field, data }), "$\u00a0200.00");
-        assert.deepEqual(formatMonetary(200, { field, currencyField: "c_y", data }), "200.00\u00a0&");
+        assert.deepEqual(
+            formatMonetary(200, { field, currencyField: "c_y", data }),
+            "200.00\u00a0&"
+        );
 
         const floatField = { type: "float" };
         data = {
@@ -195,5 +199,11 @@ QUnit.module("Fields", (hooks) => {
         assert.strictEqual(formatReference(false), "");
         const value = { resModel: "product", resId: 2, displayName: "Chair" };
         assert.strictEqual(formatReference(value), "Chair");
+    });
+
+    QUnit.test("formatBinary", function (assert) {
+        assert.strictEqual(formatBinary("1.5 MB"), "1.5 MB", "binary sizes are not modified");
+        assert.strictEqual(formatBinary("aGVsbG8="), "6 Bytes");
+        assert.strictEqual(formatBinary("a".repeat(3000)), "2.20 kB");
     });
 });
