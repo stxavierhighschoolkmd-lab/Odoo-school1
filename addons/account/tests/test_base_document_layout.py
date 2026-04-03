odoo/odoo

commit fa55c2d1ca5db203ffaa13e10e4e4209717b7a6d

[IMP] *: flexible document layout (reports)
*: base, l10n_ca, l10n_cn, l10n_cz, l10n_din5008, l10n_hu_edi,
l10n_latam_invoice_document, l10n_my_ubl_pint, l10n_ph, l10n_sa,
l10n_sk, point_of_sale, snailmail_account, web

Goal:
- Allow more flexibility on the document layout, dissociating table
designs from their `external_layout`. Users can now  mix them to create
their own designs.

- Clean up the code and use more predictable styling.

------

Important information:
- Removes `bold`,`boxed`,`striped` external_layout.

- Removes `layout_background` and `layout_background_image`
-- The feature to apply a background image on reports was unpredictable,
appearing only on the first page because we cannot compute it's ideal
position since it depends on the content of the report (+ contrast
issue, +increasing configurator complexity for niche feature).

- Adds `report_tables_id` field to select Table design.

- Striped design -> Light layout.
-- Light is now the only `snail_mail` compatible layout. It uses the
previous striped design, because it was offering more space for the
`company_address`.

- Creates `company_address_list` template to handle the company address
consistently accross the external layouts.

- Creates `external_layout_body` to handle the different tables
variations and title positioning accross the external layouts.

- Creates `external_layout_footer_content` template to call in each
layout (improve consistency and avoid xpath on each external_layout for
custom footers)

- Adapt `external_layout` styling to make them consistent with the new
table variations and combinations.

task-4916379

Part-of: odoo/odoo#237109
Related: odoo/enterprise#100312
Related: odoo/upgrade#8916
Signed-off-by: Adrien Guilliams (adgu) <adgu@odoo.com>

addons/l10n_hu_edi/views/report_templates.xml
@@ -1,5 +1,18 @@
 <?xml version="1.0" encoding="utf-8"?>
 <odoo>
+    <template id="external_layout_footer_content" inherit_id="web.external_layout_footer_content">
+        <xpath expr="//div[contains(@t-attf-class, 'o_footer_content')]" position="attributes">
+            <attribute name="t-if">not custom_footer</attribute>
+        </xpath>
+        <xpath expr="//div[contains(@t-attf-class, 'o_footer_content')]" position="after">
+            <div
+                t-attf-class="o_footer_content {{footer_content_classes}}"
+                t-if="custom_footer and company.account_fiscal_country_id.code == 'HU'"
+            >
+                <t t-out="custom_footer"/>
+            </div>
+        </xpath>
+    </template>
 
     <template id="external_layout_standard" inherit_id="web.external_layout_standard" >
         <xpath expr="//img/../.." position="attributes">
@@ -12,87 +25,6 @@
                 <t t-call="#{custom_header}"/>
             </div>
         </xpath>
-
-        <!-- support for custom footer -->
-        <div class="o_footer_content d-flex border-top pt-2" position="attributes">
-            <attribute name="t-if">not custom_footer</attribute>
-        </div>
-
-        <div class="o_footer_content d-flex border-top pt-2" position="after">
-            <div class="o_footer_content border-top pt-2"
-                 t-if="custom_footer and company.account_fiscal_country_id.code == 'HU'">
-                <t t-out="custom_footer"/>
-            </div>
-        </div>
-    </template>
-
-    <template id="external_layout_bold" inherit_id="web.external_layout_bold" >
-        <xpath expr="//img/../.." position="attributes">
-            <attribute name="t-if">not custom_header</attribute>
-        </xpath>
-
-        <xpath expr="//img/../.." position="after">
-            <div t-if="custom_header and company.account_fiscal_country_id.code == 'HU'">
-                <t t-call="#{custom_header}"/>
-            </div>
-        </xpath>
-
-        <!-- support for custom footer -->
-        <xpath expr="//div[hasclass('o_footer_content')]" position="attributes">
-            <attribute name="t-if">not custom_footer</attribute>
-        </xpath>
-        <xpath expr="//div[hasclass('o_footer_content')]" position="after">
-            <div class="o_footer_content border-top pt-2" t-attf-class="{{'d-flex' if is_html_empty(company.report_header) else 'row' }}"
-                    t-if="custom_footer and company.account_fiscal_country_id.code == 'HU'">
-                    <t t-out="custom_footer"/>
-            </div>
-        </xpath>
-    </template>
-
-    <template id="external_layout_boxed" inherit_id="web.external_layout_boxed" >
-        <xpath expr="//img/../.." position="attributes">
-            <attribute name="t-if">not custom_header</attribute>
-        </xpath>
-
-        <xpath expr="//img/../.." position="after">
-            <div t-if="custom_header and company.account_fiscal_country_id.code == 'HU'">
-                <t t-call="#{custom_header}"/>
-            </div>
-        </xpath>
-
-        <!-- support for custom footer -->
-        <xpath expr="//div[hasclass('o_footer_content')]" position="attributes">
-            <attribute name="t-if">not custom_footer</attribute>
-        </xpath>
-        <xpath expr="//div[hasclass('o_footer_content')]" position="after">
-            <div class="o_footer_content border-top pt-2" t-attf-class="{{'d-flex' if is_html_empty(company.report_header) else 'row' }}"
-                 t-if="custom_footer and company.account_fiscal_country_id.code == 'HU'">
-                <t t-out="custom_footer"/>
-            </div>
-        </xpath>
-    </template>
-
-    <template id="external_layout_striped" inherit_id="web.external_layout_striped" >
-        <xpath expr="//img/../.." position="attributes">
-            <attribute name="t-if">not custom_header</attribute>
-        </xpath>
-
-        <xpath expr="//img/../.." position="after">
-            <div t-if="custom_header and company.account_fiscal_country_id.code == 'HU'">
-                <t t-call="#{custom_header}"/>
-            </div>
-        </xpath>
-
-        <!-- support for custom footer -->
-        <div class="o_footer_content border-top pt-2 text-center" position="attributes">
-            <attribute name="t-if">not custom_footer</attribute>
-        </div>
-        <div class="o_footer_content border-top pt-2 text-center" position="after">
-            <div class="o_footer_content border-top pt-2 text-center"
-                 t-if="custom_footer and company.account_fiscal_country_id.code == 'HU'">
-                <t t-out="custom_footer"/>
-            </div>
-        </div>
     </template>
 
     <template id="external_layout_bubble" inherit_id="web.external_layout_bubble">
@@ -105,17 +37,6 @@
                 <t t-call="#{custom_header}"/>
             </div>
         </xpath>
-
-        <!-- support for custom footer -->
-        <div t-attf-class="o_footer_content {{report_type != 'pdf' and 'position-absolute end-0 start-0 bottom-0 mx-5'}} pt-4 text-center" position="attributes">
-            <attribute name="t-if">not custom_footer</attribute>
-        </div>
-        <div t-attf-class="o_footer_content {{report_type != 'pdf' and 'position-absolute end-0 start-0 bottom-0 mx-5'}} pt-4 text-center" position="after">
-            <div t-attf-class="o_footer_content {{report_type != 'pdf' and 'position-absolute end-0 start-0 bottom-0 mx-5'}} pt-4 text-center"
-                 t-if="custom_footer and company.account_fiscal_country_id.code == 'HU'">
-                <t t-out="custom_footer"/>
-            </div>
-        </div>
     </template>
 
     <template id="external_layout_wave" inherit_id="web.external_layout_wave">
@@ -127,17 +48,6 @@
         <xpath expr="//img/../.." position="after">
             <div t-if="custom_header and company.account_fiscal_country_id.code == 'HU'">
                 <t t-call="#{custom_header}"/>
-            </div>
-        </xpath>
-
-        <!-- support for custom footer -->
-        <xpath expr="//div[contains(@t-attf-class,'o_footer_content')]" position="attributes">
-            <attribute name="t-if">not custom_footer</attribute>
-        </xpath>
-        <xpath expr="//div[contains(@t-attf-class,'o_footer_content')]" position="after">
-            <div t-attf-class="o_footer_content {{report_type != 'pdf' and 'mx-5'}} pt-5 text-center"
-                 t-if="custom_footer and company.account_fiscal_country_id.code == 'HU'">
-                <t t-out="custom_footer"/>
             </div>
         </xpath>
     </template>
@@ -153,16 +63,5 @@
                 <t t-call="#{custom_header}"/>
             </div>
         </xpath>
-
-        <!-- support for custom footer -->
-        <div class="o_footer_content d-flex border-top pt-2" position="attributes">
-            <attribute name="t-if">not custom_footer</attribute>
-        </div>
-        <div class="o_footer_content d-flex border-top pt-2" position="after">
-            <div class="o_footer_content border-top pt-2"
-                 t-if="custom_footer and company.account_fiscal_country_id.code == 'HU'">
-                <t t-out="custom_footer"/>
-            </div>
-        </div>
     </template>
 </odoo>

addons/l10n_latam_invoice_document/views/report_templates.xml
@@ -1,6 +1,20 @@
 <?xml version="1.0" encoding="utf-8"?>
 <odoo>
 
+    <template id="external_layout_footer_content" inherit_id="web.external_layout_footer_content">
+        <xpath expr="//div[contains(@t-attf-class, 'o_footer_content')]" position="attributes">
+            <attribute name="t-if">not custom_footer</attribute>
+        </xpath>
+        <xpath expr="//div[contains(@t-attf-class, 'o_footer_content')]" position="after">
+            <div
+                t-attf-class="o_footer_content {{footer_content_classes}}"
+                t-if="custom_footer and company._localization_use_documents()"
+            >
+                <t t-out="custom_footer"/>
+            </div>
+        </xpath>
+    </template>
+
     <template id="external_layout_standard" inherit_id="web.external_layout_standard" >
         <!-- support for custom header -->
         <xpath expr="//img/../.." position="attributes">
@@ -12,87 +26,6 @@
                 <t t-call="#{custom_header}"/>
             </div>
         </xpath>
-
-        <!-- support for custom footer -->
-        <div class="o_footer_content d-flex border-top pt-2" position="attributes">
-            <attribute name="t-if">not custom_footer</attribute>
-        </div>
-
-        <div class="o_footer_content d-flex border-top pt-2" position="after">
-            <div class="o_footer_content border-top pt-2"
-                 t-if="custom_footer and company._localization_use_documents()">
-                <t t-out="custom_footer"/>
-            </div>
-        </div>
-    </template>
-
-    <template id="external_layout_bold" inherit_id="web.external_layout_bold" >
-        <!-- support for custom header -->
-        <xpath expr="//img/../.." position="attributes">
-            <attribute name="t-if">not custom_header</attribute>
-        </xpath>
-        <xpath expr="//img/../.." position="after">
-            <div t-if="custom_header and company._localization_use_documents()">
-                <t t-call="#{custom_header}"/>
-            </div>
-        </xpath>
-
-        <!-- support for custom footer -->
-        <xpath expr="//div[hasclass('o_footer_content')]" position="attributes">
-            <attribute name="t-if">not custom_footer</attribute>
-        </xpath>
-        <xpath expr="//div[hasclass('o_footer_content')]" position="after">
-            <div class="o_footer_content border-top pt-2" t-attf-class="{{'d-flex' if is_html_empty(company.report_header) else 'row' }}"
-                 t-if="custom_footer and company._localization_use_documents()">
-                <t t-out="custom_footer"/>
-            </div>
-        </xpath>
-    </template>
-
-    <template id="external_layout_boxed" inherit_id="web.external_layout_boxed" >
-        <!-- support for custom header -->
-        <xpath expr="//img/../.." position="attributes">
-            <attribute name="t-if">not custom_header</attribute>
-        </xpath>
-        <xpath expr="//img/../.." position="after">
-            <div t-if="custom_header and company._localization_use_documents()">
-                <t t-call="#{custom_header}"/>
-            </div>
-        </xpath>
-
-        <!-- support for custom footer -->
-        <xpath expr="//div[hasclass('o_footer_content')]" position="attributes">
-            <attribute name="t-if">not custom_footer</attribute>
-        </xpath>
-        <xpath expr="//div[hasclass('o_footer_content')]" position="after">
-            <div class="o_footer_content border-top pt-2" t-attf-class="{{'d-flex' if is_html_empty(company.report_header) else 'row' }}"
-                 t-if="custom_footer and company._localization_use_documents()">
-                <t t-out="custom_footer"/>
-            </div>
-        </xpath>
-    </template>
-
-    <template id="external_layout_striped" inherit_id="web.external_layout_striped" >
-        <!-- support for custom header -->
-        <xpath expr="//img/../.." position="attributes">
-            <attribute name="t-if">not custom_header</attribute>
-        </xpath>
-        <xpath expr="//img/../.." position="after">
-            <div t-if="custom_header and company._localization_use_documents()">
-                <t t-call="#{custom_header}"/>
-            </div>
-        </xpath>
-
-        <!-- support for custom footer -->
-        <div class="o_footer_content border-top pt-2 text-center" position="attributes">
-            <attribute name="t-if">not custom_footer</attribute>
-        </div>
-        <div class="o_footer_content border-top pt-2 text-center" position="after">
-            <div class="o_footer_content border-top pt-2 text-center"
-                 t-if="custom_footer and company._localization_use_documents()">
-                <t t-out="custom_footer"/>
-            </div>
-        </div>
     </template>
 
     <template id="external_layout_bubble" inherit_id="web.external_layout_bubble">
@@ -105,17 +38,6 @@
                 <t t-call="#{custom_header}"/>
             </div>
         </xpath>
-
-        <!-- support for custom footer -->
-        <div t-attf-class="o_footer_content {{report_type != 'pdf' and 'position-absolute end-0 start-0 bottom-0 mx-5'}} pt-4 text-center" position="attributes">
-            <attribute name="t-if">not custom_footer</attribute>
-        </div>
-        <div t-attf-class="o_footer_content {{report_type != 'pdf' and 'position-absolute end-0 start-0 bottom-0 mx-5'}} pt-4 text-center" position="after">
-            <div t-attf-class="o_footer_content {{report_type != 'pdf' and 'position-absolute end-0 start-0 bottom-0 mx-5'}} pt-4 text-center"
-                 t-if="custom_footer and company._localization_use_documents()">
-                <t t-out="custom_footer"/>
-            </div>
-        </div>
     </template>
 
     <template id="external_layout_wave" inherit_id="web.external_layout_wave">
@@ -126,17 +48,6 @@
         <xpath expr="//img/../.." position="after">
             <div t-if="custom_header and company._localization_use_documents()">
                 <t t-call="#{custom_header}"/>
-            </div>
-        </xpath>
-
-        <!-- support for custom footer -->
-        <xpath expr="//div[contains(@t-attf-class,'o_footer_content')]" position="attributes">
-            <attribute name="t-if">not custom_footer</attribute>
-        </xpath>
-        <xpath expr="//div[contains(@t-attf-class,'o_footer_content')]" position="after">
-            <div t-attf-class="o_footer_content {{report_type != 'pdf' and 'mx-5'}} pt-5 text-center"
-                 t-if="custom_footer and company._localization_use_documents()">
-                <t t-out="custom_footer"/>
             </div>
         </xpath>
     </template>
@@ -151,16 +62,5 @@
                 <t t-call="#{custom_header}"/>
             </div>
         </xpath>
-
-        <!-- support for custom footer -->
-        <div class="o_footer_content d-flex border-top pt-2" position="attributes">
-            <attribute name="t-if">not custom_footer</attribute>
-        </div>
-        <div class="o_footer_content d-flex border-top pt-2" position="after">
-            <div class="o_footer_content border-top pt-2"
-                 t-if="custom_footer and company._localization_use_documents()">
-                <t t-out="custom_footer"/>
-            </div>
-        </div>
     </template>
 </odoo>

addons/l10n_ph/views/report_templates.xml
@@ -1,55 +1,7 @@
 <?xml version="1.0" encoding="utf-8"?>
 <odoo>
-    <template id="l10n_ph.external_layout_standard" inherit_id="web.external_layout_standard">
-        <xpath expr="//div[hasclass('o_footer_content')]" position="before">
-            <t t-if="is_l10n_ph_disbursement_voucher">
-                <t t-call="l10n_ph.report_disbursement_voucher_document_footer"/>
-            </t>
-        </xpath>
-    </template>
-
-    <template id="l10n_ph.external_layout_bold" inherit_id="web.external_layout_bold">
-        <xpath expr="//div[hasclass('o_footer_content')]" position="before">
-            <t t-if="is_l10n_ph_disbursement_voucher">
-                <t t-call="l10n_ph.report_disbursement_voucher_document_footer"/>
-            </t>
-        </xpath>
-    </template>
-
-    <template id="l10n_ph.external_layout_boxed" inherit_id="web.external_layout_boxed">
-        <xpath expr="//div[hasclass('o_footer_content')]" position="before">
-            <t t-if="is_l10n_ph_disbursement_voucher">
-                <t t-call="l10n_ph.report_disbursement_voucher_document_footer"/>
-            </t>
-        </xpath>
-    </template>
-
-    <template id="l10n_ph.external_layout_striped" inherit_id="web.external_layout_striped">
-        <xpath expr="//div[hasclass('o_footer_content')]" position="before">
-            <t t-if="is_l10n_ph_disbursement_voucher">
-                <t t-call="l10n_ph.report_disbursement_voucher_document_footer"/>
-            </t>
-        </xpath>
-    </template>
-
-    <template id="l10n_ph.external_layout_bubble" inherit_id="web.external_layout_bubble">
-        <xpath expr="//div[hasclass('border-top')][@t-field='company.report_footer']" position="before">
-            <t t-if="is_l10n_ph_disbursement_voucher">
-                <t t-call="l10n_ph.report_disbursement_voucher_document_footer"/>
-            </t>
-        </xpath>
-    </template>
-
-    <template id="l10n_ph.external_layout_wave" inherit_id="web.external_layout_wave">
+    <template id="l10n_ph.external_layout_footer_content" inherit_id="web.external_layout_footer_content">
         <xpath expr="//div[@t-field='company.report_footer']" position="before">
-            <t t-if="is_l10n_ph_disbursement_voucher">
-                <t t-call="l10n_ph.report_disbursement_voucher_document_footer"/>
-            </t>
-        </xpath>
-    </template>
-
-    <template id="l10n_ph.external_layout_folder" inherit_id="web.external_layout_folder">
-        <xpath expr="//div[hasclass('o_footer_content')]" position="before">
             <t t-if="is_l10n_ph_disbursement_voucher">
                 <t t-call="l10n_ph.report_disbursement_voucher_document_footer"/>
             </t>

addons/l10n_ca/views/report_template.xml
@@ -6,43 +6,16 @@
         </li>
     </template>
 
-    <template id="l10n_ca_external_layout_standard" inherit_id="web.external_layout_standard">
+    <template id="l10n_ca_external_layout_standard" inherit_id="web.company_address_list">
         <xpath expr="//ul[@name='company_address_list']" position="inside">
-            <t t-call="l10n_ca.pst_external_layout"/>
-        </xpath>
-    </template>
-
-    <template id="l10n_ca_external_layout_bold" inherit_id="web.external_layout_bold">
-        <xpath expr="//ul[@name='company_address_list']" position="inside">
-            <t t-call="l10n_ca.pst_external_layout"/>
-        </xpath>
-    </template>
-
-    <template id="l10n_ca_external_layout_boxed" inherit_id="web.external_layout_boxed">
-        <xpath expr="//ul[@name='company_address_list']" position="inside">
-            <t t-call="l10n_ca.pst_external_layout"/>
-        </xpath>
-    </template>
-
-    <template id="l10n_ca_external_layout_striped" inherit_id="web.external_layout_striped">
-        <xpath expr="//ul[@name='company_address_list']" position="inside">
-            <t t-call="l10n_ca.pst_external_layout"/>
-        </xpath>
-    </template>
-
-    <template id="l10n_ca_external_layout_bubble" inherit_id="web.external_layout_bubble">
-        <xpath expr="//ul[@name='company_address_list']" position="inside">
-            <t t-call="l10n_ca.pst_external_layout"/>
-        </xpath>
-    </template>
-
-    <template id="l10n_ca_external_layout_wave" inherit_id="web.external_layout_wave">
-        <xpath expr="//ul[@name='company_address_list']" position="inside">
-            <t t-call="l10n_ca.pst_external_layout"/>
+            <t t-if="pst_external_layout" t-call="l10n_ca.pst_external_layout"/>
         </xpath>
     </template>
 
     <template id="l10n_ca_external_layout_folder" inherit_id="web.external_layout_folder">
+        <xpath expr="//t[@t-call='web.company_address_list']" position="before">
+            <t t-set="pst_external_layout" t-value="False"/>
+        </xpath>
         <xpath expr="//div[hasclass('o_folder_company_info')]" position="after">
             <div t-if="company.account_fiscal_country_id.code == 'CA' and company.l10n_ca_pst">
                 PST: <span t-field="company.l10n_ca_pst"/>

addons/l10n_cn/views/report_voucher.xml
@@ -1,8 +1,9 @@
 <?xml version="1.0" encoding="utf-8"?>
 <odoo>
     <data>
-        <template id="external_layout_boxed" inherit_id="web.external_layout_boxed" primary="True">
-            <xpath expr="//div[contains(@t-attf-class, 'header')]/div" position="replace">
+        <!-- TODO test if it's the intended result -->
+        <template id="external_layout_standard" inherit_id="web.external_layout_standard" primary="True">
+            <xpath expr="//div[contains(@t-attf-class, 'header')]/table" position="replace">
                 <div class="row mb8">
                     <div class="col-3 mb4">
                         <img t-if="company.logo" t-att-src="image_data_uri(company.logo)" style="max-height: 45px;" alt="Logo"/>
@@ -15,7 +16,7 @@
             <t t-set="o" t-value="o.with_context(lang=lang)" />
             <t t-set="company" t-value="o.company_id"/>
 
-            <t t-call="l10n_cn.external_layout_boxed">
+            <t t-call="l10n_cn.external_layout_standard">
                 <div class="page">
 
                     <div align="center">

addons/l10n_cz/views/report_template.xml
@@ -11,43 +11,16 @@
         </li>
     </template>
 
-    <template id="l10n_cz_external_layout_standard" inherit_id="web.external_layout_standard">
+    <template id="l10n_cz_external_layout_standard" inherit_id="web.company_address_list">
         <xpath expr="//ul[@name='company_address_list']" position="inside">
-            <t t-call="l10n_cz.registry_vat_external_layout"/>
-        </xpath>
-    </template>
-
-    <template id="l10n_cz_external_layout_bold" inherit_id="web.external_layout_bold">
-        <xpath expr="//ul[@name='company_address_list']" position="inside">
-            <t t-call="l10n_cz.registry_vat_external_layout"/>
-        </xpath>
-    </template>
-
-    <template id="l10n_cz_external_layout_boxed" inherit_id="web.external_layout_boxed">
-        <xpath expr="//ul[@name='company_address_list']" position="inside">
-            <t t-call="l10n_cz.registry_vat_external_layout"/>
-        </xpath>
-    </template>
-
-    <template id="l10n_cz_external_layout_striped" inherit_id="web.external_layout_striped">
-        <xpath expr="//ul[@name='company_address_list']" position="inside">
-            <t t-call="l10n_cz.registry_vat_external_layout"/>
-        </xpath>
-    </template>
-
-    <template id="l10n_cz_external_layout_bubble" inherit_id="web.external_layout_bubble">
-        <xpath expr="//ul[@name='company_address_list']" position="inside">
-            <t t-call="l10n_cz.registry_vat_external_layout"/>
-        </xpath>
-    </template>
-
-    <template id="l10n_cz_external_layout_wave" inherit_id="web.external_layout_wave">
-        <xpath expr="//ul[@name='company_address_list']" position="inside">
-            <t t-call="l10n_cz.registry_vat_external_layout"/>
+            <t t-if="registry_vat_external_layout" t-call="l10n_cz.registry_vat_external_layout"/>
         </xpath>
     </template>
 
     <template id="l10n_cz_external_layout_folder" inherit_id="web.external_layout_folder">
+        <xpath expr="//t[@t-call='web.company_address_list']" position="before">
+            <t t-set="registry_vat_external_layout" t-value="False"/>
+        </xpath>
         <xpath expr="//div[hasclass('o_folder_company_info')]" position="after">
             <div t-if="company.company_registry and company.account_fiscal_country_id.code == 'CZ'">
                 Company ID: <span t-field="company.company_registry"/>

addons/l10n_din5008/report/din5008_report.xml
@@ -44,12 +44,8 @@
                         </tr>
                     </table>
                 </div>
-                <t t-set="layout_background_url"
-                   t-value="'data:image/png;base64,%s' % company.layout_background_image.decode('utf-8') if company.layout_background_image and company.layout_background == 'Custom' else
-                        '/base/static/img/demo_logo_report.png' if company.layout_background == 'Demo logo' else ''" />
-                
-                <div t-attf-class="din_page invoice_note article o_company_#{company.id}_layout {{'o_report_layout_background' if company.layout_background in ['Geometric', 'Custom']  else  ''}} #{'din_page_pdf' if report_type == 'pdf' and is_din_paperformat else ''}"
-                     t-attf-style="{{ 'background-image: url(%s);' % layout_background_url if layout_background_url else '' }}"
+
+                <div t-attf-class="din_page invoice_note article o_company_#{company.id}_layout #{'din_page_pdf' if report_type == 'pdf' and is_din_paperformat else ''}"
                      t-att-data-oe-model="o and o._name"
                      t-att-data-oe-id="o and o.id">
                     <table class="din_company_info table-borderless">

stxavierhighschoolkmd-lab/Odoo-school1

commit c84a79a88e50b564a8540829bb579ff22823dccb

[FIX] web: show company's TIN in documents
Steps to reproduce:
===================
- Download Accounting app
- Open 'Settings' > 'Companies' > 'Document Layout' > 'Configure Document Layout'
- Enter a 'Tax ID' and click continue

> 'Tax ID' doesn't show in the document

Cause of issue:
===============
The problem was introduced by https://github.com/odoo-dev/odoo/commit/fa55c2d1ca5db203ffaa13e10e4e4209717b7a6d, as the commited changes don't use the `company.vat` set by the user to be displayed in the documents.

Fix:
====
Simplified the whole block by computing a single value to display, where `forced_vat`
takes precedence over `company.vat` as before.

opw-5981630

addons/web/views/report_templates.xml
@@ -378,15 +378,10 @@
                     </div>
                 </span>
             </li>
-            <li t-if="not forced_vat">
-                <t t-if="company.vat">
-                    <t t-out="company.country_id.vat_label or 'Tax ID'">Tax ID</t>:
-                    <span t-out="company.vat">US12345671</span>
-                </t>
-            </li>
-            <li t-else="">
-                <t t-out="company.country_id.vat_label or 'Tax ID'">Tax ID</t>:
-                <span t-out="forced_vat">US12345671</span>
+            <t t-set="vat_number" t-value="forced_vat or company.vat"/>
+            <li t-if="vat_number">
+                <t t-out="company.country_id.vat_label or 'Tax ID'"/>:
+                <span t-out="vat_number"/>
             </li>
         </ul>
     </template>

addons/account/tests/__init__.py
@@ -67,3 +67,4 @@ from . import test_account_bill_deductibility
 from . import test_dict_to_xml
 from . import test_duplicate_res_partner_bank
 from . import test_account_move_import_template
+from . import test_base_document_layout

addons/account/tests/test_base_document_layout.py
@@ -1,0 +1,21 @@
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo.tests.common import TransactionCase, tagged
+
+
+@tagged('post_install', '-at_install')
+class TestBaseDocumentLayout(TransactionCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+
+    def test_company_vat_appears_in_documents(self):
+        self.env.company.vat = 'BE987654321'
+        rendered = self.env['ir.ui.view']._render_template(
+            'web.company_address_list',
+            {
+                'company': self.env.company,
+                'forced_vat': False,
+            },
+        )
+        self.assertIn('BE987654321', rendered)